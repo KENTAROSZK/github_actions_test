2aac70dadb3b46afe54cbc5d412e6ffd91a5775e
鈴木 健太郎 <szkkntru2@gmail.com>
2024-10-24 09:52:16 +0900

    change python.yml

--- src/sum_array.py
+++ src/sum_array.py
@@ -1,7 +1,7 @@
 import os
 
 # 環境変数から出力ファイル名を取得
-file_name = os.getenv('INPUT_FILE', 'numbers.txt')
+file_name = os.getenv('INPUT_FILE', 'output.txt')
 
 # output.txtファイルを読み取る
 with open(file_name, 'r') as f:
